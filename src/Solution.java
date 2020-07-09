1dd89dceb772b35df1411b62ab3ea5524b50a3cc
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-07-09 23:00:10 +0700

    Add the solution code

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Solution {
+
+    private static int B;
+    private static int H;
+    private static boolean flag;
+
+    static {
+        Scanner scanner = new Scanner(System.in);
+        B = scanner.nextInt();
+        H = scanner.nextInt();
+        flag = true;
+
+        if (B < 0 || H < 0) {
+            flag = false;
+            System.out.print("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = B * H;
+            System.out.print(area);
+        }
+    }
 }
